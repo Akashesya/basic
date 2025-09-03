1962382da5ffae0afab547bdb6f583b628c91864
Akashesya <Akash.agrahari@esyasoft.com>
2025-09-03 14:32:13 +0530

    push python file

---
+++ Python/main.py
@@ -0,0 +1,13 @@
+# my_int =43
+# my_list=[10,"mangi"]
+# print(my_int)
+# print(my_list)
+
+side = int(input("Enter the side length of the square: "))
+area = side * side
+print(area)
+
+# x=10
+# y=20
+# z=x*y
+# print(z)
