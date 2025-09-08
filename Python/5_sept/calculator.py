6aa694b13c00c11942a02620887bcac027f85efd
Akashesya <Akash.agrahari@esyasoft.com>
2025-09-08 15:31:28 +0530

    load dataset

---
+++ Python/5_sept/calculator.py
@@ -0,0 +1,9 @@
+def add(x,y):
+    return x+y
+def subtraction(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divison(x,y):
+    return x/y
+    
---
+++ Python/5_sept/demo.py
@@ -0,0 +1,6 @@
+import calculator as calc
+
+print(calc.add(12,5))
+print(calc.subtraction(12,5))
+print(calc.multiply(12,5))
+print(calc.divison(12,5))
